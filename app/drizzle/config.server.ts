6c4a470b6cb4ff8148ef9dfb145b3521e24a271e
Hans Hofner <hhofner@gmail.com>
2023-12-27 11:57:51 +0900

    migrate to turso

--- app/drizzle/config.server.ts
+++ app/drizzle/config.server.ts
@@ -1,6 +1,9 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import Database from "better-sqlite3";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+
+// import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { migrate } from "drizzle-orm/libsql/migrator";
+// import Database from "better-sqlite3";
 import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -8,8 +11,18 @@ dotenv.config();
 if (!process.env.DATABASE_PATH) {
   throw new Error("Missing environment variable: DATABASE_PATH");
 }
-console.log(process.env.DATABASE_PATH);
-export const db = drizzle(new Database(process.env.DATABASE_PATH));
+if (!process.env.AUTH_TOKEN) {
+  throw new Error("Missing environment variable: AUTH_TOKEN");
+}
+
+const client = createClient({
+  url: process.env.DATABASE_PATH,
+  authToken: process.env.AUTH_TOKEN,
+});
+
+export const db = drizzle(client);
+
+// export const db = drizzle(new Database(process.env.DATABASE_PATH));
 // Automatically run migrations on startup
 void migrate(db, {
   migrationsFolder: "app/drizzle/migrations",
@@ -17,9 +30,10 @@ void migrate(db, {
 
 export default {
   schema: "./schema.server.ts",
-  driver: "better-sqlite",
+  driver: "turso",
   dbCredentials: {
-    url: `sqlite:///${process.env.DATABASE_PATH}`,
+    url: process.env.DATABASE_PATH,
+    authToken: process.env.AUTH_TOKEN,
   },
   verbose: true,
 } satisfies Config;
