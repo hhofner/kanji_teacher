33b4660538f18082b22e7dce12f034c9e1ab76e7
Hans Hofner <hhofner@gmail.com>
2024-01-26 15:45:15 +0900

    update about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -3,17 +3,9 @@ export default function About() {
     <div className="space-y-6">
       <h1 className="text-3xl">About: How It Works</h1>
       <p>
-        Oh My Kanji is a self-study Kanji app. The main focus of this
-        application is to help you learn Kanji by writing more. In Japan, school
-        children learn Kanji by writing them over and over again. While some may
-        find this method tedious, it can be effective for some.
-      </p>
-      <p>
-        Oh My Kanji also has an opiniated approach. The user is meant to choose
-        what Kanjis they will take time to learn. This is done by selecting a
-        set of Kanjis for the week. The user can then study these Kanjis until
-        the end of the week where they then test themselves and then move on to
-        another set.
+        Oh My Kanji is a simple application meant to encourage you to write Kanji characters more. It is meant
+        to be used as a companion application to a book or application that has a structured set of Kanjis. Oh My Kanji
+        expects the user to select a set of Kanji characters they will study for the week.
       </p>
       <p>
         Additionally, Oh My Kanji will provide a set of other features that
@@ -21,21 +13,20 @@ export default function About() {
         planned features. While not all of these features are implemented yet,
         they are planned for the future.
       </p>
-      <h2 className="text-2xl">Roadmap (in no particular order)</h2>
+      <h2 className="text-2xl">Planned features (in no particular order)</h2>
       <ul className="space-y-2">
         <li>End of the week Kanji tests</li>
         <li>Individual Kanji Pages</li>
         <li>Study Kanji meanings</li>
         <li>Study Kanji readings</li>
         <li>Study Kanji vocabulary</li>
-        <li></li>
-        <li></li>
+        <li>Kanji sets</li>
+        <li>More statistics</li>
       </ul>
       <p>
         If you're interested in this project and would like to see it developed
-        further (ie, as a native app, or an API), then consider donating,
-        sending me a message on X or Mastodon or giving this project a thumbs
-        up.
+        further (ie, as a native app, an API, or anything else), then consider <a href="https://www.buymeacoffee.com/hhofner">donating</a>,
+        sending me a message on <a href="https://twitter.com/hofner_pls">X</a> or <a href="https://mozilla.social/@hans">Mastodon</a>.
       </p>
     </div>
   );
