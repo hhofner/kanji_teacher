d3d7a47d2539082e530c70b21866779c9bb01319
Hans Hofner <hhofner@gmail.com>
2024-01-26 15:52:34 +0900

    fix remix-pwa issue

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: [
+    /@remix-pwa\/.*/,
+  ],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
